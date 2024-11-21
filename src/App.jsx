9bb3794c66d5db4555a8ae46db5f8d462a424de9
fayorr <fayokunmiosho@gmail.com>
2024-11-21 01:05:49 +0100

    added styledcomponents

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import AboutUs from './pages/AboutUs'
+import GlobalStyle from './components/GlobalStyle'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <GlobalStyle />
       <AboutUs />
     </>
   )
