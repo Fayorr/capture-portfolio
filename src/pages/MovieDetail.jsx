f8061bfa996d4bec8e222eadce67f0b448979c58
fayorr <fayokunmiosho@gmail.com>
2024-11-28 05:14:56 +0100

    added router

---
+++ src/pages/MovieDetail.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const MovieDetail = () => {
+  return (
+    <div>
+        <h1>MovieDetail</h1>
+    </div>
+  )
+}
+
+export default MovieDetail
