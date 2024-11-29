375a71ea0f4e3db6fc1139b1a13992f8cc30d0c3
godstime <agholorgodstime18@gmail.com>
2024-11-29 22:03:03 +0100

    Fixed route issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,30 @@
-import './App.css'
-import AboutUs from './pages/AboutUs'
-import ContactUs from './pages/ContactUs'
-import OurWork from './pages/OurWork'
-import MovieDetail from './pages/MovieDetail'
-import GlobalStyle from './components/GlobalStyle'
-import Nav from './components/Nav'
-import {Routes, Route} from 'react-router-dom'
+import './App.css';
+import AboutUs from './pages/AboutUs';
+import ContactUs from './pages/ContactUs';
+import OurWork from './pages/OurWork';
+import MovieDetail from './pages/MovieDetail';
+import GlobalStyle from './components/GlobalStyle';
+import Nav from './components/Nav';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
+	return (
+		<>
+			<Nav />
+			<GlobalStyle />
+			<Routes>
+				<Route path="/" element={<AboutUs />} />
+				<Route path="/about" element={<AboutUs />} />
+				<Route path="/work">
+					<Route index element={<OurWork />} />
+					<Route path="/work/:id" element={<MovieDetail />} />
+				</Route>
 
-  return (
-    <>
-      <Nav />
-      <GlobalStyle />
-      <Routes>
-        <Route path='/' element={<AboutUs />} />
-        <Route path='/about' element={<AboutUs />} />
-        <Route path='/work'>
-          <Route index element={<OurWork />} />
-          <Route path=':id' element={<MovieDetail />} />
-        </Route>
- 
-        <Route path='/contact' element={<ContactUs />} />
-        <Route path='*' element={<AboutUs />} />
-
-
-      </Routes>
-      
-    </>
-  )
+				<Route path="/contact" element={<ContactUs />} />
+				<Route path="*" element={<AboutUs />} />
+			</Routes>
+		</>
+	);
 }
 
-export default App
+export default App;
