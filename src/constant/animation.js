92ae149d5728e05f807543109b0dce844e3fa45b
fayorr <fayokunmiosho@gmail.com>
2024-12-28 04:59:45 +0100

    added more animation

--- src/constant/animation.js
+++ src/constant/animation.js
@@ -49,3 +49,19 @@ export const lineAnim = {
     show: {width: "100%",
     transition: {duration: 1}},
 }
+export const slider = {
+    hidden: {x: "-130%", skew: "45deg"},
+    show:{
+        x: "100%",
+        skew: "0deg",
+        transition: {ease: "easeOut", duration: 1}
+    }
+}
+export const sliderContainer = {
+    hidden: {opacity: 1},
+    show:{
+        opacity: 1,
+        transition: {ease: "easeOut", duration: 0.1, staggerChildren:0.1}
+        
+    }
+}
--- src/pages/OurWork.jsx
+++ src/pages/OurWork.jsx
@@ -3,11 +3,15 @@ import { Link } from 'react-router-dom';
 import { MovieState } from '../constant/movieState';
 import { motion } from "framer-motion"
 import { AnimatePresence } from "framer-motion"
-import { pageAnimation, fade, photoAnim, lineAnim } from '../constant/animation';
+import { pageAnimation, fade, photoAnim, lineAnim, slider } from '../constant/animation';
 
 const OurWork = () => {
 	return (
 		<Work variants={pageAnimation} initial="hidden" animate="show" exit="exit">
+				<Frame1 variants={slider}></Frame1>
+				<Frame2 variants={slider}></Frame2>
+				<Frame3 variants={slider}></Frame3>
+				<Frame4 variants={slider}></Frame4>
 			<AnimatePresence>
 				{MovieState.map((movie, index) => (
 					<Movie key={index} variants={pageAnimation}>
@@ -48,5 +52,26 @@ const Movie = styled.div`
 `;
 const Hide = styled.div`
 	overflow: hidden;
+`;
+
+//Frame Animation 
+const Frame1 = styled(motion.div)`
+	position: fixed;
+	left: 0;
+	top: 10%;
+	width: 100%;
+	height: 100vh;
+	background: #fffebf;
+	z-index: 2;
+`;
+const Frame2 = styled(Frame1)`
+	background: #ff8efb;
 `
+const Frame3 = styled(Frame1)`
+	background: #8ed2ff;
+`
+const Frame4 = styled(Frame1)`
+	background: #8effa0;
+`
+
 export default OurWork;
